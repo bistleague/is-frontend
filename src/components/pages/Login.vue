b4429b2d3a2212352328e8f6c3e273c6d8e4b3f2
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-04-18 18:52:17 +0700

    Fixed login and sign up

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,43 +1,41 @@
 <template>
-    <div>
-        <v-container bg fill-height grid-list-md text-xs-center>
-            <v-layout row wrap align-center>
-                <v-flex>
-                    <div align="center">
-                        <v-card id="login-box" elevation="8">
-                            <v-img :src="require('../../assets/logo.png')" width="100"></v-img>
-                            <div class="headline" style="margin-top: 24px">Welcome!</div>
-                            <p>Type in your email and password to sign in</p>
+    <v-container bg fill-height grid-list-md text-xs-center flex class="primary darken-1">
+        <v-layout row wrap align-center>
+            <v-flex>
+                <div align="center">
+                    <v-card id="login-box" elevation="8">
+                        <v-img :src="require('../../assets/logo.png')" width="100"></v-img>
+                        <div class="headline" style="margin-top: 24px">Welcome!</div>
+                        <p>Type in your email and password to sign in</p>
 
-                            <div style="height: 48px"></div>
-                            <v-text-field label="Email address"></v-text-field>
-                            <v-text-field label="Password" type="password"></v-text-field>
-                            <div style="height: 8px"></div>
+                        <div style="height: 48px"></div>
+                        <v-text-field label="Email address"></v-text-field>
+                        <v-text-field label="Password" type="password"></v-text-field>
+                        <div style="height: 8px"></div>
 
-                            <v-layout pa-0 fill-height>
-                                <v-flex xs6>
-                                    <v-layout row wrap align-center fill-height>
-                                        <v-flex>
-                                            <div align="left">
-                                                <a>Forgot password?</a>
-                                                <br />
-                                                <router-link to="register" style="text-decoration: none;">New user? <b>Sign up</b></router-link>
-                                            </div>
-                                        </v-flex>
-                                    </v-layout>
-                                </v-flex>
-                                <v-flex xs6 pa-0>
-                                    <div align="right">
-                                        <v-btn ma-0 depressed style="text-transform: none" color="primary">Sign in</v-btn>
-                                    </div>
-                                </v-flex>
-                            </v-layout>
-                        </v-card>
-                    </div>
-                </v-flex>
-            </v-layout>
-        </v-container>
-    </div>
+                        <v-layout pa-0 fill-height>
+                            <v-flex xs6>
+                                <v-layout row wrap align-center fill-height>
+                                    <v-flex>
+                                        <div align="left">
+                                            <a>Forgot password?</a>
+                                            <br />
+                                            <router-link to="register" style="text-decoration: none;">New user? <b>Sign up</b></router-link>
+                                        </div>
+                                    </v-flex>
+                                </v-layout>
+                            </v-flex>
+                            <v-flex xs6 pa-0>
+                                <div align="right">
+                                    <v-btn ma-0 depressed style="text-transform: none" color="primary">Sign in</v-btn>
+                                </div>
+                            </v-flex>
+                        </v-layout>
+                    </v-card>
+                </div>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
--- src/components/pages/SignUp.vue
+++ src/components/pages/SignUp.vue
@@ -1,41 +1,39 @@
 <template>
-    <div>
-        <v-container bg fill-height grid-list-md text-xs-center>
-            <v-layout row wrap align-center>
-                <v-flex>
-                    <div align="center">
-                        <v-card id="login-box" elevation="8">
-                            <v-img :src="require('../../assets/logo.png')" width="100"></v-img>
-                            <div class="headline" style="margin-top: 24px">Sign up</div>
-                            <p>Hi there! Let’s get to know you first</p>
+    <v-container bg fill-height grid-list-md text-xs-center flex class="primary darken-1">
+        <v-layout row wrap align-center>
+            <v-flex>
+                <div align="center">
+                    <v-card id="login-box" elevation="8">
+                        <v-img :src="require('../../assets/logo.png')" width="100"></v-img>
+                        <div class="headline" style="margin-top: 24px">Sign up</div>
+                        <p>Hi there! Let’s get to know you first</p>
 
-                            <v-text-field label="Full name"></v-text-field>
-                            <v-text-field label="Email address"></v-text-field>
-                            <v-text-field label="Password" type="password"></v-text-field>
-                            <div style="height: 8px"></div>
+                        <v-text-field label="Full name"></v-text-field>
+                        <v-text-field label="Email address"></v-text-field>
+                        <v-text-field label="Password" type="password"></v-text-field>
+                        <div style="height: 8px"></div>
 
-                            <v-layout pa-0 fill-height>
-                                <v-flex xs6>
-                                    <v-layout row wrap align-center fill-height>
-                                        <v-flex>
-                                            <div align="left">
-                                                <router-link to="login" style="text-decoration: none;">Have an account? <b>Sign in</b></router-link>
-                                            </div>
-                                        </v-flex>
-                                    </v-layout>
-                                </v-flex>
-                                <v-flex xs6 pa-0>
-                                    <div align="right">
-                                        <v-btn ma-0 depressed style="text-transform: none" color="primary">Sign up</v-btn>
-                                    </div>
-                                </v-flex>
-                            </v-layout>
-                        </v-card>
-                    </div>
-                </v-flex>
-            </v-layout>
-        </v-container>
-    </div>
+                        <v-layout pa-0 fill-height>
+                            <v-flex xs6>
+                                <v-layout row wrap align-center fill-height>
+                                    <v-flex>
+                                        <div align="left">
+                                            <router-link to="login" style="text-decoration: none;">Have an account? <b>Sign in</b></router-link>
+                                        </div>
+                                    </v-flex>
+                                </v-layout>
+                            </v-flex>
+                            <v-flex xs6 pa-0>
+                                <div align="right">
+                                    <v-btn ma-0 depressed style="text-transform: none" color="primary">Sign up</v-btn>
+                                </div>
+                            </v-flex>
+                        </v-layout>
+                    </v-card>
+                </div>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
