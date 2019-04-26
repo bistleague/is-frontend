f6a372d7aa59ee648ba70d0e42c6389ca24a6cb9
didithilmy <didithilmy@gmail.com>
2019-04-26 17:46:10 +0700

    Added enter for login submit

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -4,41 +4,43 @@
             <v-flex>
                 <div align="center">
                     <v-card id="login-box" elevation="8">
-                        <v-alert v-model="alert" color="warning" icon="warning" dismissible>
-                            <div align="left">
-                                {{error}}
+                        <v-form @submit.prevent="login">
+                            <v-alert v-model="alert" color="warning" icon="warning" dismissible>
+                                <div align="left">
+                                    {{error}}
+                                </div>
+                            </v-alert>
+
+                            <div class="pa-4">
+                                <v-img :src="require('../../assets/logo.png')" width="100"></v-img>
+                                <div class="headline" style="margin-top: 24px">Welcome!</div>
+                                <p>Type in your email and password to sign in</p>
+
+                                <div style="height: 48px"></div>
+                                <v-text-field label="Email address" v-model="email"></v-text-field>
+                                <v-text-field label="Password" type="password" v-model="password"></v-text-field>
+                                <div style="height: 8px"></div>
+
+                                <v-layout pa-0 fill-height>
+                                    <v-flex xs6>
+                                        <v-layout row wrap align-center fill-height>
+                                            <v-flex>
+                                                <div align="left">
+                                                    <a>Forgot password?</a>
+                                                    <br />
+                                                    <router-link to="register" style="text-decoration: none;">New user? <b>Sign up</b></router-link>
+                                                </div>
+                                            </v-flex>
+                                        </v-layout>
+                                    </v-flex>
+                                    <v-flex xs6 pa-0>
+                                        <div align="right">
+                                            <v-btn ma-0 depressed style="text-transform: none" color="primary" :loading="loading" type="submit">Sign in</v-btn>
+                                        </div>
+                                    </v-flex>
+                                </v-layout>
                             </div>
-                        </v-alert>
-
-                        <div class="pa-4">
-                            <v-img :src="require('../../assets/logo.png')" width="100"></v-img>
-                            <div class="headline" style="margin-top: 24px">Welcome!</div>
-                            <p>Type in your email and password to sign in</p>
-
-                            <div style="height: 48px"></div>
-                            <v-text-field label="Email address" v-model="email"></v-text-field>
-                            <v-text-field label="Password" type="password" v-model="password"></v-text-field>
-                            <div style="height: 8px"></div>
-
-                            <v-layout pa-0 fill-height>
-                                <v-flex xs6>
-                                    <v-layout row wrap align-center fill-height>
-                                        <v-flex>
-                                            <div align="left">
-                                                <a>Forgot password?</a>
-                                                <br />
-                                                <router-link to="register" style="text-decoration: none;">New user? <b>Sign up</b></router-link>
-                                            </div>
-                                        </v-flex>
-                                    </v-layout>
-                                </v-flex>
-                                <v-flex xs6 pa-0>
-                                    <div align="right">
-                                        <v-btn ma-0 depressed style="text-transform: none" color="primary" :loading="loading" @click="login">Sign in</v-btn>
-                                    </div>
-                                </v-flex>
-                            </v-layout>
-                        </div>
+                        </v-form>
                     </v-card>
                 </div>
             </v-flex>
