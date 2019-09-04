04c7866314f86bcb40640cf5c95a17087ddc3c71
didithilmy <didithilmy@gmail.com>
2019-09-04 22:07:57 +0700

    Add document profile UI

--- src/components/pages/admin/DocumentProfile.vue
+++ src/components/pages/admin/DocumentProfile.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
-        <BLToolbar />
+        <BLToolbar/>
         <BLSubHeader>
             <div class="headline font-weight-bold white--text mt-3 mb-3">User Document Profile</div>
         </BLSubHeader>
         <v-content class="">
+            <div align="center" v-if="loading" class="pa-4">
+                <v-progress-circular indeterminate color="primary"></v-progress-circular>
+            </div>
             <BLCenterWrap v-if="!loading">
                 <v-layout wrap mt-5>
-                    <v-flex xs12 sm6>
+                    <v-flex xs12 sm12>
                         <v-form ref="form">
                             <div class="headline">{{ team.name }}</div>
                             <div class="title mt-1 grey--text">{{ team.university }}</div>
@@ -26,6 +29,9 @@
                                                     </v-flex>
                                                 </v-layout>
                                                 <div class="mt-3">Student ID</div>
+                                                <div v-if="!item.student_id_file_url">
+                                                    <b class="red--text">Not uploaded</b>
+                                                </div>
                                                 <div v-if="item.student_id_file_url" class="mt-1">
                                                     <v-layout align-center>
                                                         <v-flex shrink>
@@ -35,12 +41,26 @@
                                                             <a target="_blank" :href="item.student_id_file_url">{{item.student_id_filename}}</a>
                                                         </v-flex>
                                                         <v-flex shrink>
+                                                            <div v-if="student_id_loading">
+                                                                <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                                                            </div>
+                                                            <div v-if="!student_id_loading">
+                                                                <v-btn outline small color="red" @click="() => studentIdReject(item.user_id)" v-if="!item.student_id_rejected">
+                                                                    Reject
+                                                                </v-btn>
+                                                                <v-btn outline small color="green" @click="() => studentIdValidate(item.user_id)" v-if="!item.student_id_verified">
+                                                                    Accept
+                                                                </v-btn>
+                                                            </div>
                                                         </v-flex>
                                                     </v-layout>
                                                 </div>
 
                                                 <div class="mt-3">
                                                     Proof of enrollment
+                                                </div>
+                                                <div v-if="!item.poe_file_url">
+                                                    <b class="red--text">Not uploaded</b>
                                                 </div>
                                                 <div v-if="item.poe_file_url" class="mt-1">
                                                     <v-layout align-center>
@@ -51,7 +71,18 @@
                                                             <a target="_blank" :href="item.poe_file_url">{{item.poe_filename}}</a>
                                                         </v-flex>
                                                         <v-flex shrink>
-                                                       </v-flex>
+                                                            <div v-if="poe_loading">
+                                                                <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                                                            </div>
+                                                            <div v-if="!poe_loading">
+                                                                <v-btn small outline color="red" @click="() => poeReject(item.user_id)" v-if="!item.poe_rejected">
+                                                                    Reject
+                                                                </v-btn>
+                                                                <v-btn small outline color="green" @click="() => poeValidate(item.user_id)" v-if="!item.poe_verified">
+                                                                    Accept
+                                                                </v-btn>
+                                                            </div>
+                                                        </v-flex>
                                                     </v-layout>
                                                 </div>
                                             </v-card-text>
@@ -77,6 +108,7 @@
     import BLToolbar from "../../partials/BLToolbar";
     import BLSubHeader from "../../partials/BLSubHeader";
     import BLCenterWrap from "../../partials/BLCenterWrap";
+
     const axios = require('axios');
 
     export default {
@@ -85,6 +117,8 @@
         data() {
             return {
                 loading: false,
+                poe_loading: false,
+                student_id_loading: false,
                 snackbar: false,
                 snackbar_text: '',
                 snackbar_color: 'success',
@@ -92,11 +126,11 @@
                 users: []
             }
         },
-        mounted: function() {
+        mounted: function () {
             this.load();
         },
         methods: {
-            show_snackbar: function(text, color) {
+            show_snackbar: function (text, color) {
                 this.snackbar = true;
                 this.snackbar_text = text;
                 this.snackbar_color = color;
@@ -109,8 +143,8 @@
                     .then((response) => {
                         const data = response.data;
 
-                        if(data.error) {
-                            this.handleError(data.error);
+                        if (data.error) {
+                            this.show_snackbar(data.error.toString(), 'error');
                         }
 
                         this.team = data.team;
@@ -122,13 +156,92 @@
                     .finally(() => {
                         this.loading = false;
                     });
+            },
+            poeValidate(userId) {
+                this.poe_loading = true;
+                this.postStatus(userId, 'poe', 'validate', (success) => {
+                    this.poe_loading = false;
+                    if(success) {
+                        this.changePoeVerified(userId, true);
+                        this.changePoeRejected(userId, false);
+                    }
+                });
+            },
+            poeReject(userId) {
+                this.poe_loading = true;
+                this.postStatus(userId, 'poe', 'reject', (success) => {
+                    this.poe_loading = false;
+                    if(success) {
+                        this.changePoeVerified(userId, false);
+                        this.changePoeRejected(userId, true);
+                    }
+                });
+            },
+            studentIdValidate(userId) {
+                this.student_id_loading = true;
+                this.postStatus(userId, 'student-id', 'validate', (success) => {
+                    this.student_id_loading = false;
+                    if(success) {
+                        this.changeStudentIdVerified(userId, true);
+                        this.changeStudentIdRejected(userId, false);
+                    }
+                });
+            },
+            studentIdReject(userId) {
+                this.student_id_loading = true;
+                this.postStatus(userId, 'student-id', 'reject', (success) => {
+                    this.student_id_loading = false;
+                    if(success) {
+                        this.changeStudentIdVerified(userId, false);
+                        this.changeStudentIdRejected(userId, true);
+                    }
+                });
+            },
+            postStatus(userId, doc, action, onFinished) {
+                axios.post(`${process.env.VUE_APP_API_BASE_URL}/v1/admin/documents/${doc}/${action}`, {user_id: userId}, {
+                    headers: {'Authorization': `Bearer ${this.$store.getters.jwt}`},
+                })
+                    .then((response) => {
+                        const data = response.data;
+
+                        if (data.error) {
+                            this.show_snackbar(data.error.toString(), 'error');
+                        }
+
+                        onFinished(true);
+                    })
+                    .catch((err) => {
+                        this.show_snackbar(err.toString(), 'error');
+                        onFinished(false);
+                    });
+            },
+            changePoeVerified(userId, verified) {
+                const user = this.findUser(userId);
+                user.poe_verified = verified;
+            },
+            changePoeRejected(userId, rejected) {
+                const user = this.findUser(userId);
+                user.poe_rejected = rejected;
+            },
+            changeStudentIdVerified(userId, verified) {
+                const user = this.findUser(userId);
+                user.student_id_verified = verified;
+            },
+            changeStudentIdRejected(userId, rejected) {
+                const user = this.findUser(userId);
+                user.student_id_rejected = rejected;
+            },
+            findUser(userId) {
+                return this.users.find((user) => {
+                    return user.user_id === userId;
+                });
             }
         }
     }
 </script>
 
 <style scoped>
-    .radio-group-full-width >>>.v-input__control {
+    .radio-group-full-width >>> .v-input__control {
         width: 100%
     }
 
